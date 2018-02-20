0fd28bbdc52578d3a0ab728c4332984901cfabde
daniel <da>
2018-02-20 17:46:27 -0500

    add new project: claro api messaging

---
+++ claro-sales-order/claro-api-messaging/src/main/java/pe/com/claro/App.java
@@ -0,0 +1,13 @@
+package pe.com.claro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
