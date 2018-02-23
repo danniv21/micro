f559fbf7c38df047fa3dbe6bc3e5a407b0dc4b74
daniel <da>
2018-02-23 17:00:10 -0500

    update 23-02-2018

--- claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/sales/order/domain/service/CustomerService.java
+++ claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/sales/order/domain/service/CustomerService.java
@@ -4,6 +4,7 @@ package pe.com.claro.sales.order.domain.service;
 
 import pe.com.claro.common.resource.exception.EntityNotFoundException;
 
+import org.eclipse.jetty.util.thread.ExecutionStrategy.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,16 @@ public class CustomerService {
 	private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
 	@Autowired
 	private CustomerRepository customerRepository;
+	@Autowired
+	private pe.com.claro.sales.order.message.component.Producer producer;
+	
 	public Customer getPostCustomer(Long customerId) throws EntityNotFoundException {
 		logger.debug("Get post " + customerId);
 		Customer customer = customerRepository.findOne(customerId);
 		if (customer == null) {
 			 throw new EntityNotFoundException(Customer.class, "id", customerId.toString());
 		}
+		producer.produce(customer);
 		return customer;
 	}
 }
