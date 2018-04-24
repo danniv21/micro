f7566cf3851106834e885ba71a41992033811bb2
danniv21 <danniv21@gmail.com>
2018-04-24 17:35:06 -0500

    commit

--- claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/sales/order/service/CustomerService.java
+++ claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/sales/order/service/CustomerService.java
@@ -1,7 +1,5 @@
 package pe.com.claro.sales.order.service;
 
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,34 +14,34 @@ import pe.com.claro.sales.order.model.Customer;
 import pe.com.claro.sales.order.repository.CustomerRepository;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
-
 @Service
 @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED, rollbackFor = Exception.class)
 public class CustomerService {
-	private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
-	@Autowired
-	private CustomerRepository customerRepository;
-	@Autowired
-	private pe.com.claro.sales.order.message.component.Producer producer;
-	
-	@Value("${application.appname.customerservice.message001}")
-	private String message001;
-	
-	
-	@Transactional(readOnly = true)
-//	@HystrixCommand(fallbackMethod = "getTokenHystrixFallbackMethod")
-	public Customer getPostCustomer(Long customerId){
-		logger.debug("Get post " + customerId);
-		Customer customer = customerRepository.findOne(customerId);
-		if (customer == null) {
-			 throw new CustomEntityNotFoundException(message001 + " "+customerId.toString());
-		}
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private pe.com.claro.sales.order.message.component.Producer producer;
+
+    @Value("${application.appname.customerservice.message001}")
+    private String message001;
+
+    @Transactional(readOnly = true)
+    @HystrixCommand(fallbackMethod = "getTokenHystrixFallbackMethod")
+    public Customer getPostCustomer(Long customerId) {
+        logger.debug("Get post " + customerId);
+        Customer customer = customerRepository.findOne(customerId);
 //		producer.produce(customer);
-		return customer;
-	}
-	public Customer getTokenHystrixFallbackMethod(Long customerId) {
+        return customer;
+    }
+
+    public Customer getTokenHystrixFallbackMethod(Long customerId) {
         logger.error("Error en el Sistema Intente Nuevamente ");
         Customer customer = null;
+        if (customer == null) {
+            throw new CustomEntityNotFoundException(message001 + " " + customerId.toString());
+        }
         return customer;
     }
 }
