590d399a81f0bff0c6207dd4813b857b438e94db
danniv21 <danniv21@gmail.com>
2018-05-15 10:40:12 -0500

    commit

--- claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/Application.java
+++ claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/Application.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableCircuitBreaker
 public class Application {
 
     public static void main(String[] args) {
