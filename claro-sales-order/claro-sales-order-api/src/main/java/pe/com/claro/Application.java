015c6f87cd27efe482b111c1468da85ad4ae4fcc
danniv21 <danniv21@gmail.com>
2018-05-15 10:25:53 -0500

    remove eureka client

--- claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/Application.java
+++ claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/Application.java
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableDiscoveryClient
-@EnableCircuitBreaker
 @SpringBootApplication
 public class Application {
 
