b620dc579b7d2fc1f508b5e42becc410af62b5ae
E751023 <E751023@PST02HITSS140LP.tim.com.pe>
2018-06-25 11:59:02 -0500

    update code

--- claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/sales/order/message/component/Producer.java
+++ claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/sales/order/message/component/Producer.java
@@ -1,7 +1,5 @@
 package pe.com.claro.sales.order.message.component;
 
-//import com.springbootdev.samples.producer.model.Item;
-//import com.springbootdev.samples.producer.model.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -24,29 +22,6 @@ public class Producer
     {
         LOGGER.info(" Publishing the Customer " + customer.getId());
         Message msg= new Message(customer.getCustomerName() + " "+ customer.getCustomerLastName());
-        
-     //   Order order = this.getOrder();
-      rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, msg);
+        rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, msg);
     }
-
-	
-
-/*
-    private Order getOrder()
-    {
-        List<Item> orderItemList = new ArrayList<>();
-        Item item1 = new Item("Phone", 1);
-        orderItemList.add(item1);
-
-        Item item2 = new Item("SIM", 1);
-        orderItemList.add(item2);
-
-        Item item3 = new Item("Charger", 1);
-        orderItemList.add(item3);
-
-        return new Order("Chathuranga Tennakoon", new Date(), "www.springbootdev.com", orderItemList);
-    }*/
-
-
-
 }
--- claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/sales/order/repository/CustomerRepository.java
+++ claro-sales-order/claro-sales-order-api/src/main/java/pe/com/claro/sales/order/repository/CustomerRepository.java
@@ -9,6 +9,4 @@ import pe.com.claro.sales.order.model.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-//	List<Customer> findByOriginAndDestinationAndFlightDate(String origin,String destination, String flightDate);
-	//Customer findByFlightNumberAndFlightDate(String flightNumber, String flightDate);
 } 
